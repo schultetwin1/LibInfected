08095ca0d3e05d088ab1e23ef81776bc8fe3845b
Matt Schulte <schultetwin1@gmail.com>
2015-04-23 21:51:04 -0400

    Initial Commit

---
+++ LibInfected/LibInfected.h
@@ -0,0 +1,13 @@
+//
+//  LibInfected.h
+//  LibInfected
+//
+//  Created by Matt Schulte on 4/23/15.
+//  Copyright (c) 2015 Matt Schulte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LibInfected : NSObject
+
+@end
---
+++ LibInfected/LibInfected.m
@@ -0,0 +1,13 @@
+//
+//  LibInfected.m
+//  LibInfected
+//
+//  Created by Matt Schulte on 4/23/15.
+//  Copyright (c) 2015 Matt Schulte. All rights reserved.
+//
+
+#import "LibInfected.h"
+
+@implementation LibInfected
+
+@end
